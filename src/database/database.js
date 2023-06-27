d06a1ce0c989ad21376c74d498405b1275978860
Junio Lemos <junio.lemos@hotmail.com>
2023-06-27 11:46:15 -0300

    Feat conexaõ com o database

---
+++ src/database/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+function connectToDatabase() {
+    mongoose.connect('mongodb://127.0.0.1:27017', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+
+    }).then(() => {
+        console.log("Mongo DB Conectado");
+
+    }).catch((err) => {
+        return console.log(`Erro na conexão com o banco: ${err}`);
+    })
+
+}
+
+module.exports = connectToDatabase
